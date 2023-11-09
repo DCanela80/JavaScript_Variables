117adf754113d0959c0e74197e47d69106f5fef0
Diana Canela <DCanela80@gmail.com>
2023-11-09 13:37:23 -0700

    updated exercise

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
+let lastName = "Canela";
+var age = 43;
 
 // Exercise 2
+const firstName = "Diana";
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitivev = "true";
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = "false"
+let salePercentage = 15;
+let stock = 0;
+let inStock = "false";
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let authour = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = "true";
